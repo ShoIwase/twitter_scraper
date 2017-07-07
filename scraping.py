eb7012c4b96c08f80e8df19701c16ee67d017a8a
ShoIwase <i.shokun1021@gmail.com>
2017-07-07 10:09:40 +0900

    ファイル追加

---
+++ scraping.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+import time
+import sys
+from urllib import urlencode
+from selenium import webdriver
+
+argv = sys.argv
+argc = len(argv)
+print argv
+print argc
+
+url = 'https://twitter.com/search?f=tweets&q=github%20lang%3Aja%20since%3A2016-'+str(argv[1])+'-'+str(argv[2])+'_00%3A00%3A00_JST%20until%3A2016-'+str(argv[3])+'-'+str(argv[4])+'_00%3A00%3A00_JST&src=typd'
+
+print url
+
+# ブラウザ起動
+driver = webdriver.Firefox()
+
+# twitter検索結果表示長い方
+driver.get(url)
+
+# 一番下までスクロール
+lastHeight = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    newHeight = driver.execute_script("return document.body.scrollHeight")
+    if newHeight == lastHeight:
+        break
+    lastHeight = newHeight
+    
+# HTML出力
+data = driver.page_source.encode('utf-8')
+print(data)
+
+# スクリーンショットを撮る
+driver.save_screenshot('sys_result.png')
+
+# 終了
+driver.close()
