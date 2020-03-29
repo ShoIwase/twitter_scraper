18cab2ea9178147be9395cc16f4cd52dcdb8f734
Sho IWASE <i.shokun1021@gmail.com>
2020-03-30 00:54:34 +0900

    数年ぶりにアップデート
    このままでは不十分なので続く

--- scroll.py
+++ scroll.py
@@ -1,17 +1,17 @@
 # coding: utf-8
 import time
-import sys
-from urllib import urlencode
+import urllib3
 from selenium import webdriver
 
-argv = sys.argv
-argc = len(argv)
-print argv
-print argc
+# コマンドラインから検索ワードを設定
+searchWord = input("検索ワード：")
+startDay = input("開始日（YYYYMMDD）：")
+endDay = input("終了日（YYYYMMDD）：")
 
-url = 'https://twitter.com/search?f=tweets&q=github%20lang%3Aja%20since%3A'+str(argv[1])+'-'+str(argv[2])+'-'+str(argv[3])+'_00%3A00%3A00_JST%20until%3A'+str(argv[1])+'-'+str(argv[4])+'-'+str(argv[5])+'_00%3A00%3A00_JST&src=typd'
+url = 'https://twitter.com/search?f=tweets&q='+searchWord+'%20lang%3Aja%20since%3A'+startDay[0:4]+'-'+startDay[5:6]+'-'+startDay[7:8]+'_00%3A00%3A00_JST%20until%3A'+endDay[0:4]+'-'+endDay[5:6]+'-'+endDay[7:8]+'_00%3A00%3A00_JST&src=typd'
 
-print url
+print("URL："+url)
+print("以上の設定で取得します．．．")
 
 # ブラウザ起動
 driver = webdriver.Firefox()
@@ -23,18 +23,19 @@ driver.get(url)
 lastHeight = driver.execute_script("return document.body.scrollHeight")
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(3)
+    time.sleep(10)
     newHeight = driver.execute_script("return document.body.scrollHeight")
     if newHeight == lastHeight:
         break
     lastHeight = newHeight
     
 # HTML出力
-data = driver.page_source.encode('utf-8')
-print(data)
+html = driver.page_source
+with open(searchWord+'_'+startDay+'_'+endDay+'.html', 'w', encoding='utf-8') as f:
+    f.write(html)
 
 # 確認のためスクリーンショットを撮る
-driver.save_screenshot('result.png')
+driver.save_screenshot(searchWord+'_'+startDay+'_'+endDay+'.png')
 
 # 終了
 driver.close()
