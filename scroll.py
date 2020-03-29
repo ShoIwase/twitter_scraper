595f54788f86eb0cbb3b0ef28b4b1b043c20a02d
Sho IWASE <i.shokun1021@gmail.com>
2020-03-30 01:26:22 +0900

    ミスってた

--- scroll.py
+++ scroll.py
@@ -8,7 +8,7 @@ searchWord = input("検索ワード：")
 startDay = input("開始日（YYYYMMDD）：")
 endDay = input("終了日（YYYYMMDD）：")
 
-url = 'https://twitter.com/search?f=tweets&q='+searchWord+'%20lang%3Aja%20since%3A'+startDay[0:4]+'-'+startDay[5:6]+'-'+startDay[7:8]+'_00%3A00%3A00_JST%20until%3A'+endDay[0:4]+'-'+endDay[5:6]+'-'+endDay[7:8]+'_00%3A00%3A00_JST&src=typd'
+url = 'https://twitter.com/search?f=tweets&q='+searchWord+'%20lang%3Aja%20since%3A'+startDay[0:4]+'-'+startDay[4:6]+'-'+startDay[6:8]+'_00%3A00%3A00_JST%20until%3A'+endDay[0:4]+'-'+endDay[4:6]+'-'+endDay[6:8]+'_00%3A00%3A00_JST&src=typd'
 
 print("URL："+url)
 print("以上の設定で取得します．．．")
@@ -20,11 +20,14 @@ driver = webdriver.Firefox()
 driver.get(url)
 
 # 一番下までスクロール
+#count = 0
 lastHeight = driver.execute_script("return document.body.scrollHeight")
 while True:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(10)
     newHeight = driver.execute_script("return document.body.scrollHeight")
+#    driver.save_screenshot('scroll'+str(count)+'.png')
+#    count += 1
     if newHeight == lastHeight:
         break
     lastHeight = newHeight
